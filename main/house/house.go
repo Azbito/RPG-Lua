072b7f41b034b7b43626fef7e3368d831cd702dc
Thiago <101950809+Azbito@users.noreply.github.com>
2023-04-21 20:54:52 -0300

    first commit

---
+++ main/house/house.go
@@ -0,0 +1,15 @@
+components {
+  id: "house"
+  component: "/main/house/house.tilemap"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
---
+++ main/map/overworld.go
@@ -0,0 +1,15 @@
+components {
+  id: "overworldMap"
+  component: "/main/map/overworldMap.tilemap"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
